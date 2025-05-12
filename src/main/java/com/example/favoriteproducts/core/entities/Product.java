4c0b9072c3149e07295192d03a6f35f4fe82c818
felipenobrg <felipenobrega2012@gmail.com>
2025-05-12 18:31:46 -0300

    feat: create product entity

---
+++ src/main/java/com/example/favoriteproducts/core/entities/Product.java
@@ -0,0 +1,10 @@
+package com.example.favoriteproducts.core.entities;
+
+public class Product {
+    private final String id;
+    private final String title;
+    private final String brand;
+    private final double price;
+    private final String image;
+    private final Double reviewScore;
+}
